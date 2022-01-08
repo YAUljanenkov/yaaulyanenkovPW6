4312a74ab52cdb199a225d6c85d69747132ef19c
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2022-01-08 23:57:11 +0300

    Create project

---
+++ yaaulyanenkovPW6/ButtonsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ButtonsViewController.swift
+//  yaaulyanenkovPW6
+//
+//  Created by Ярослав Ульяненков on 08.01.2022.
+//
+
+import UIKit
+
+class ButtonsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
